2cd9a3c3d6fa7440cfbb3d989e7faac610c7053c
Ercel  Williams <ercelwilliams@Ercels-MacBook-Air.local>
2020-10-24 21:28:46 -0400

    spanish

---
+++ spanish.py
@@ -0,0 +1,11 @@
+word = input("Give me a word in English... ");
+if word == "cat" or word == "Cat":
+	print ("gato")
+
+elif word == "horse" or word == "Horse":
+	print("caballo")
+
+elif word == "dog" or word == "Dog":
+	print("perro")
+else:
+	print("no entiendo")
